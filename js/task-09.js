786f8a527a209f5967c9ab68d35d9afffb8d98d7
snackli17 <snackli17@gmail.com>
2023-02-27 00:50:06 +0200

    fix
    fix

--- js/task-09.js
+++ js/task-09.js
@@ -9,3 +9,12 @@ function getRandomHexColor() {
   <p>Background color: <span class="color">-</span></p>
   <button type="button" class="change-color">Change color</button>
 </div> */}
+
+const backgroundColor = document.querySelector("body")
+const titleColor = document.querySelector(".color");
+
+const changeButton = document.querySelector(".change-color")
+changeButton.addEventListener("click", (event) => {
+  backgroundColor.style.backgroundColor = getRandomHexColor();
+  titleColor.textContent = getRandomHexColor();
+})
