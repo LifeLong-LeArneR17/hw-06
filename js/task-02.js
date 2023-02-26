b4515f91f483cd04bac6fd8351a2282a025dce4c
snackli17 <snackli17@gmail.com>
2023-02-26 19:04:57 +0200

    fix2
    fix2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const List = document.querySelector("ul#ingredients")
+console.log(List);
+for (const el of ingredients) {
+  const ListIngredients = document.createElement("li");
+  ListIngredients.textContent = el;
+  ListIngredients.classList.add("item");
+  List.append(ListIngredients)
+}
+
